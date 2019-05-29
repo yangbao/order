2faf4d30c983f2a8612395321259227915c50491
yangbao <yangbaobgkp@sina.com>
2019-05-29 12:34:21 +0800

    上次提交 只是在order 用git add . 有遗漏

---
+++ api-gateway/src/main/java/com/imooc/apigateway/fliter/AuthSellerFilter.java
@@ -0,0 +1,71 @@
+package com.imooc.apigateway.fliter;
+
+import com.imooc.apigateway.constant.RedisConstant;
+import com.imooc.apigateway.util.CookieUtil;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * 权限拦截（区分买家和卖家）
+ * Created by 廖师兄
+ * 2018-02-15 15:34
+ */
+@Component
+public class AuthSellerFilter extends ZuulFilter {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        if ("/order/order/finish".equals(request.getRequestURI())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        /**
+         * /order/finish 只能卖家访问(cookie里有token, 并且对应的redis中值)
+         */
+
+        Cookie cookie = CookieUtil.get(request, "token");
+        if (cookie == null
+                || StringUtils.isEmpty(cookie.getValue())
+                || StringUtils.isEmpty(stringRedisTemplate.opsForValue().get(String.format(RedisConstant.TOKEN_TEMPLATE, cookie.getValue())))) {
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+
+        return null;
+    }
+}
+
